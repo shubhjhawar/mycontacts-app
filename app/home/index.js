f9c911d002ae267c947a020bc296f3df0b5eeaf5
Shubh <shubhjhawar78@gmail.com>
2023-08-22 18:04:06 -0400

    data getting fetched successfully in the first go

--- app/home/index.js
+++ app/home/index.js
@@ -7,6 +7,27 @@ import axios from 'axios';
 
 import styles from '../../styles/search';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
 const Home = () => {
   const router = useRouter();
   const [username, setUsername] = useState("");
@@ -18,7 +39,6 @@ const Home = () => {
   useEffect(() => {
     const getUserInfo = async () => {
       const token = await AsyncStorage.getItem("AccessToken");
-      setApiToken(token);
       const decodedToken = jwtDecode(token);
       setUsername(decodedToken.user.username[0].toUpperCase());
     }
@@ -27,31 +47,84 @@ const Home = () => {
     
   }, [])
 
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        const response =  await axios.get(
-          "http://192.168.0.16:5001/api/contacts",
-          {
-            headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiToken}`
-            }
-          }
-        )
-        setData(response.data);
-        setIsData(true);
-        // console.log("data fetched")
-      } catch(error) {
-        console.log(error);
-        setIsData(false);
+  const getContacts = async () => {
+    await fetch('http://192.168.0.16:5001/api/contacts', {
+      method:"GET",
+      headers:{
+        'Authorization': 'Bearer ' + apiToken
       }
+    })
+    .then((response) => {
+      if(!response.ok)
+      {
+        console.log("not ok")
+        return response.json().then(data => {
+          console.log(data);
+        })
+      } else {
+        console.log("ok");
+        return response.json().then(data => {
+          setData(data);
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    const getToken = async () => {
+      const token = await AsyncStorage.getItem("AccessToken");
+      setApiToken(token);
+      console.log(apiToken);
     }
 
-    getContacts();
-    // console.log(data);
 
-  }, [isData, data])
+    getToken();
+
+    if(apiToken)
+    {
+      console.log("api call")
+      getContacts();
+    }
+
+  }, [apiToken])
+  
+  
+
+  // useEffect(() => {
+  //   const MAX_RETRIES = 3; // Maximum number of retries
+  //   let retries = 0;
+  
+  //   const getContactsWithRetry = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "http://192.168.0.16:5001/api/contacts",
+  //         {
+  //           headers: {
+  //             'Authorization': 'Bearer ' + apiToken
+  //           }
+  //         }
+  //       );
+  //       console.log("API call successful");
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.log("API call failed:", error);
+  //       if (retries < MAX_RETRIES) {
+  //         const delay = Math.pow(2, retries) * 1000; // Increase delay exponentially
+  //         console.log(`Retrying in ${delay}ms...`);
+  //         retries++;
+  //         setTimeout(getContactsWithRetry, delay);
+  //       } else {
+  //         console.log("Max retries reached. Unable to fetch data.");
+  //       }
+  //     }
+  //   };
+  
+  //   if (!isData) {
+  //     getContactsWithRetry();
+  //   }
+  // }, []);
+  
   
 
   const handleLogout = async () => {
@@ -85,21 +158,17 @@ const Home = () => {
           </TouchableOpacity>
         )}
       </View>
-      <ScrollView >
-        <View style={styles.container}>
+      <View >
+        <View >
           <Text style={styles.heading}>My Contacts</Text>
         </View>
-        {/* <FlatList 
-          // data={data}
-          data={[1,2,4,5,6,7,8,9]}
-          renderItem={({item})=>(
-            <Text >{item}</Text>
-          )}
-          keyExtractor={item => item.toString()}
-          
-        /> */}
+        <FlatList
+        data={data}
+        renderItem={({item}) => <Item title={item.email || "herere"} />}
+        keyExtractor={item => item?._id}
+      />
         
-      </ScrollView>
+      </View>
     </SafeAreaView>
   )
 }
